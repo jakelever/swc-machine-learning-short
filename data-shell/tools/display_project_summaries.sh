fb56ea396b970aeb18b05bc17fde4ea5711009a3
Jake Lever <jlever@bcgsc.ca>
2017-04-24 17:33:11 -0700

    Added example script

---
+++ data-shell/tools/display_project_summaries.sh
@@ -0,0 +1,6 @@
+cat research_files/project_gibson/summary.txt
+cat research_files/project_arcturus/summary.txt
+cat research_files/project_columbia/summary.txt
+cat research_files/project_asimov/summary.txt
+cat research_files/project_titan/summary.txt
+cat research_files/project_smith/summary.txt
