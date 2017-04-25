63b6d7a52d498a13fe74b357e4ba08793f542d76
Jake Lever <jlever@bcgsc.ca>
2017-04-24 17:54:39 -0700

    Updated to finance_results.txt

--- data-shell/tools/finance_summary.sh
+++ data-shell/tools/finance_summary.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [[ $# -eq 1 ]]; then
 	if [[ "$1" == "--help" ]] ; then
-		echo "This tool summaries financial statements. It expects a directory called 'tmp_statements' in the current directory. The statements folder should contain a series of statement files. And it will output the results to the file 'results.tsv"
+		echo "This tool summaries financial statements. It expects a directory called 'tmp_statements' in the current directory. The statements folder should contain a series of statement files. And it will output the results to the file 'finance_results.txt'"
 		exit 0
 	else
 		echo "ERROR: Unknown argument: $1"
@@ -15,8 +15,8 @@ if [ ! -d tmp_statements/ ]; then
 	echo "ERROR: Could not find tmp_statements/ directory"
 	exit 1
 fi
-if [ -f results.tsv ]; then
-	echo "ERROR: results.tsv file already exists"
+if [ -f finance_results.txt ]; then
+	echo "ERROR: finance_results.txt file already exists"
 	exit 1
 fi
 
@@ -29,8 +29,8 @@ fi
 
 cat tmp_statements/*_statement.txt |\
 awk -F '$' ' { summary[$1] += $2; } END { for (company in summary) print company"\t$ "summary[company]; } ' |\
-sort -k2,2nr -t $'$' > results.tsv
+sort -k2,2nr -t $'$' > finance_results.txt
 
-echo "Summary saved to results.tsv using $statementCount statement(s) from the following account(s):"
+echo "Summary saved to finance_results.txt using $statementCount statement(s) from the following account(s):"
 find tmp_statements/ -name '*_statement.txt' | grep -oP "ACCNO_[0-9]*" | sort -u
 
