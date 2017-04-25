f8c5eaf2a066cffad5e457dc4b4f9f603689f5a9
Jake Lever <jlever@bcgsc.ca>
2017-04-25 00:41:58 -0700

    Better usage for encrypt

--- data-generation/encrypt.sh
+++ data-generation/encrypt.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[ $# -ne 2 ]]; then
+	echo "USAGE: sh encrypt.sh FILE PASSWORD"
+	exit 1
+fi
+
 inF=$1
 password=$2
 outF=tmp
@@ -10,4 +15,6 @@ echo "$password" | tr '[A-Z]' '[X-ZA-W]' | tr '[a-z]' '[x-za-w]' >> $outF
 cat $inF | tr '[A-Z]' '[X-ZA-W]' | tr '[a-z]' '[x-za-w]' >> $outF
 
 mv $outF $inF
+
+echo "Encrypted."
 
