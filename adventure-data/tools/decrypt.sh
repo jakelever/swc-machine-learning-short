5dbbb39e66e9024d309ff5e4895206fdaf2c452e
Jake Lever <jlever@bcgsc.ca>
2017-04-24 23:04:21 -0700

    Fix decrypt tool

--- adventure-data/tools/decrypt.sh
+++ adventure-data/tools/decrypt.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ $# -neq 2 ]]; then
+if [[ $# -ne 2 ]]; then
 	echo "USAGE: sh decrypt.sh PASSWORD FILE"
 	echo
 	echo "PASSWORD: The password used to encrypt the file"
 	echo "FILE: The file to decrypt"
+	exit 1
 fi
 
 password=$1
 inF=$2
 
 decryptedPassword=`head -n 2 $inF | tail -n 1 | tr '[X-ZA-W]' '[A-Z]' | tr '[x-za-w]' '[a-z]'`
-if [[ "$password" == "$decryptedPassword" ]]; then
+if [[ "$password" != "$decryptedPassword" ]]; then
 	echo "Password is incorrect"
 	exit 1
 fi
