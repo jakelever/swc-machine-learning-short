18694192d7eab8a60dbbc8541219fb9aa7b147fe
Jake Lever <jlever@bcgsc.ca>
2017-04-24 22:59:41 -0700

    Updated encrypt tool

--- data-generation/encrypt.sh
+++ data-generation/encrypt.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 inF=$1
 password=$2
-outF=$3
+outF=tmp
 
 echo "ENCRYPTED" > $outF
 echo "$password" | tr '[A-Z]' '[X-ZA-W]' | tr '[a-z]' '[x-za-w]' >> $outF
 cat $inF | tr '[A-Z]' '[X-ZA-W]' | tr '[a-z]' '[x-za-w]' >> $outF
+
+mv $outF $inF
 
