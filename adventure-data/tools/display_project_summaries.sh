093a077415c59504fdea911336b284d489274596
Jake Lever <jake.lever@gmail.com>
2019-10-10 15:43:07 -0700

    Lots of file removal from old lesson

--- adventure-data/tools/display_project_summaries.sh
+++
@@ -1,7 +0,0 @@
-# Displays the summary of various projects
-cat research_files/project_gibson/summary.txt
-cat research_files/project_arcturus/summary.txt
-cat research_files/project_columbia/summary.txt
-cat research_files/project_asimov/summary.txt
-cat research_files/project_titan/summary.txt
-cat research_files/project_smith/summary.txt
